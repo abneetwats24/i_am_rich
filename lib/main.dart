11dcf4505af376f547863b0ec13326a90b9f3130
Abneet Wats <abneetwats24@gmail.com>
2021-07-22 19:36:46 +0530

    Network Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text("Hello World"),),));
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(child: Image(
+        image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+      ),)
+    ),
+  ));
 }
